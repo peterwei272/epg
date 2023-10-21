d25eba99882dce54cc3f86963babdeb40e707111
Wentao Wei <67835782+peterwei272@users.noreply.github.com>
2023-10-21 18:04:26 +0800

    Update jms.js

--- jms.js
+++ jms.js
@@ -6,17 +6,22 @@ if (headers['x-emby-authorization'] == undefined) {
 } else {
     var a = 'x-emby-authorization'
 }
+if (headers['user-agent'] == undefined) {
+    var b = 'User-Agent'
+} else {
+    var b = 'user-agent'
+}
 var start = 10 + headers[a].indexOf('DeviceId="')
 var end = headers[a].indexOf('"', start+1)
 let deviceid = headers[a].slice(start,end)
 if (url.indexOf('Users/AuthenticateByName') != -1) {
-    headers['user-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15';
+    headers[b] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15';
     headers[a] = 'MediaBrowser Version="4.7.14.0", Client="Emby Web", Device="Safari macOS", DeviceId="'+deviceid+'"';
 } else {
     var start = 7+headers[a].indexOf('Token="');
     var end = headers[a].indexOf('"', start+1);
     var token = headers[a].slice(start,end);
-    headers['user-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15';
-headers[a] = 'MediaBrowser Version="4.7.14.0", Token="'+token+'", Client="Emby Web", Device="Safari macOS", DeviceId="'+deviceid+'"';
+    headers[b] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15';
+    headers[a] = 'MediaBrowser Version="4.7.14.0", Token="'+token+'", Client="Emby Web", Device="Safari macOS", DeviceId="'+deviceid+'"';
 }
 $done({headers});

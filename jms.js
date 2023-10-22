7499ef842c124b4871c3b02c475d0a2322e15a3c
Wentao Wei <67835782+peterwei272@users.noreply.github.com>
2023-10-22 16:05:50 +0800

    Update jms.js

--- jms.js
+++ jms.js
@@ -10,19 +10,20 @@ if (headers['user-agent'] == undefined) {
 } else {
     var b = 'user-agent'
 }
-if (headers[a] != undefined && headers[a].indexOf('Infuse') != -1 || headers[b] != undefined && headers[b].indexOf('Infuse') != -1) {
+if (headers[a] != undefined && headers[a].indexOf('Infuse') != -1) {
     var start = 10 + headers[a].indexOf('DeviceId="')
     var end = headers[a].indexOf('"', start+1)
     let deviceid = headers[a].slice(start,end)
-    if (url.indexOf('Users/AuthenticateByName') != -1) {
-        headers[b] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15';
-        headers[a] = 'MediaBrowser Version="4.7.14.0", Client="Emby Web", Device="Safari macOS", DeviceId="'+deviceid+'"';
-    } else {
-        var start = 7+headers[a].indexOf('Token="');
-        var end = headers[a].indexOf('"', start+1);
-        var token = headers[a].slice(start,end);
-        headers[b] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15';
+    if (headers[a].indexOf('Token="') != -1) {
+        start = 7+headers[a].indexOf('Token="');
+        end = headers[a].indexOf('"', start+1);
+        token = headers[a].slice(start,end);
         headers[a] = 'MediaBrowser Version="4.7.14.0", Token="'+token+'", Client="Emby Web", Device="Safari macOS", DeviceId="'+deviceid+'"';
+    } else {
+        headers[a] = 'MediaBrowser Version="4.7.14.0", Client="Emby Web", Device="Safari macOS", DeviceId="'+deviceid+'"';
     }
+}
+if (headers[b] != undefined && headers[b].indexOf('Infuse') != -1) {
+    headers[b] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15';
 }
 $done({headers});
